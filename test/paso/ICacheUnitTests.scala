e91befc3eed1ac15e791b24b5a5ae13adec9012c
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-12-17 17:52:58 -0800

    try to elaborate the icache

--- test/paso/ICacheUnitTests.scala
+++ test/paso/ICacheUnitTests.scala
@@ -1,40 +1,51 @@
 package paso
 
-
 import chipsalliance.rocketchip.config._
 import chisel3._
 import chiseltest._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.rocket._
-import freechips.rocketchip.subsystem.{CacheBlockBytes, RocketTilesKey, SystemBusKey, WithoutTLMonitors}
+import freechips.rocketchip.subsystem._
 import freechips.rocketchip.system.DefaultConfig
-import freechips.rocketchip.tile.{RocketTileParams, TileKey}
+import freechips.rocketchip.tile._
+import freechips.rocketchip.tilelink._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class ICacheWrapper(cacheParams: ICacheParams, p: Parameters) extends Module {
+class ICacheStandalone(cacheParams: ICacheParams, params: Parameters) extends LazyModule()(params) {
+
   val cache = LazyModule(new ICache(cacheParams, 0)(p))
-  val dut = cache.module
-  val io = IO(chiselTypeOf(dut.io))
-  io <> dut.io
+
+  // extract port parameters
+  val portParams = cache.masterNode.portParams.head
+
+  val buffer = TLBuffer(BufferParams.default)
+  cache.masterNode := buffer
+
+  val bridge = BundleBridgeToTL(portParams)
+  buffer := bridge
+  val ioInNode = BundleBridgeSource(() => TLBundle(TLBundleParameters(portParams, bridge.edges.out.head.slave)))
+  bridge := ioInNode
+  val in = InModuleBody { ioInNode.makeIO() }
+  lazy val module = new LazyModuleImp(this) {}
 }
 
-
 class ICacheUnitTests extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "ICache"
-
-
-
+  behavior.of("ICache")
 
   val DefaultConfig = new DefaultConfig
   val DefaultTileParams: RocketTileParams = DefaultConfig(RocketTilesKey).head
-  val DefaultWithTileKey = DefaultConfig.alterMap(Map(TileKey -> DefaultTileParams))
+  // inspired by BaseTile constructor
+  val DefaultWithLegacyParams = DefaultConfig.alterMap(
+    Map(
+      TileKey -> DefaultTileParams,
+      TileVisibilityNodeKey -> TLEphemeralNode()(ValName("tile_master"))
+    )
+  )
   val DefaultICacheParams: ICacheParams = DefaultTileParams.icache.get
-
 
   it should "elaborate" in {
 
-    test(new ICacheWrapper(DefaultICacheParams, DefaultWithTileKey)) { dut =>
-
-    }
+    val dut = LazyModule(new ICacheStandalone(DefaultICacheParams, DefaultWithLegacyParams))
+    test(dut.module) { dut => }
   }
 }
